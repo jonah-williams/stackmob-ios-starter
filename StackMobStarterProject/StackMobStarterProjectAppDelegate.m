5efca90ba14d8f932038639a5fabd25b4137138c
Douglas Rapp <drapp@stackmob.com>
2012-03-06 13:41:49 -0800

    end session only at application termination

--- StackMobStarterProject/StackMobStarterProjectAppDelegate.m
+++ StackMobStarterProject/StackMobStarterProjectAppDelegate.m
@@ -36,7 +36,6 @@
      Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
      If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
      */
-    [[StackMob stackmob] endSession];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
@@ -44,7 +43,6 @@
     /*
      Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
      */
-    [[StackMob stackmob] startSession];
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
