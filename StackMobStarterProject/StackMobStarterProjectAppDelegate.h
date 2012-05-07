d9d0e97cb85a25af9750b1053f20fadd0f7050c2
Douglas Rapp <drapp@stackmob.com>
2012-05-07 11:16:54 -0700

    Update to latest inneractive sdk

--- StackMobStarterProject/StackMobStarterProjectAppDelegate.h
+++ StackMobStarterProject/StackMobStarterProjectAppDelegate.h
@@ -8,8 +8,25 @@
 
 #import <UIKit/UIKit.h>
 
-@interface StackMobStarterProjectAppDelegate : UIResponder <UIApplicationDelegate>
+@interface StackMobStarterProjectAppDelegate : UIResponder <UIApplicationDelegate, UIAlertViewDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
+
+- (IBAction)iaAdRecieved:(id)sender;
+- (IBAction)iaDefaultAdRecieved:(id)sender;
+- (IBAction)iaAdFailed:(id)sender;
+- (IBAction)iaAdClicked:(id)sender;
+- (IBAction)iaAdWillShow:(id)sender;
+- (IBAction)iaAdDidShow:(id)sender;
+- (IBAction)iaAdWillHide:(id)sender;
+- (IBAction)iaAdDidHide:(id)sender;
+- (IBAction)iaAdWillClose:(id)sender;
+- (IBAction)iaAdDidClose:(id)sender;
+- (IBAction)iaAdWillResize:(id)sender;
+- (IBAction)iaAdDidResize:(id)sender;
+- (IBAction)iaAdWillExpand:(id)sender;
+- (IBAction)iaAdDidExpand:(id)sender;
+- (IBAction)iaAppShouldSuspend:(id)sender;
+- (IBAction)iaAppShouldResume:(id)sender;
 
 @end
